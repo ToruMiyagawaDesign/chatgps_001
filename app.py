6a4008d7cdf3f1bd5ccc30712d025bd7261f9639
ToruMiyagawaDesign <tm230203@gmail.com>
2024-01-14 15:34:49 +0700

    1st commit from vscode

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def communicate():
 
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.write("ChatGPS,新世代のアイドル型ChatAI")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
